eefa5a475ecf7b7d82b46118b81bf396bd2c9dce
Bradley <brad@Bradleys-MacBook-Pro.local>
2018-03-01 13:51:49 -1000

    set up redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk';
+import {createStore,applyMiddleware,compose} from 'redux';
+import reducers from './reducers'
 import './index.css';
-import App from './App';
+import App from './containers/app/App'
 import registerServiceWorker from './registerServiceWorker';
+const boundCompose = compose.bind(null,applyMiddleware(thunk));const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? boundCompose(window.__REDUX_DEVTOOLS_EXTENSION__()) : boundCompose()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
