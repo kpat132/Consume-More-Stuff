effa3d792d8712a6d3c7b51b8258e04fff354691
KrishnaP <kpat132@gmail.com>
2018-03-01 11:26:41 -1000

    setup

---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      user: '',
+      password: 'password',
+      database: '',
+      charset: 'utf8'
+    },
+    migrations: {
+      directory: path.join(__dirname + “/knex/migrations”)
+    },
+    seeds: {
+      directory: path.join(__dirname + “/knex/seeds”)
+    },
+    debug: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
