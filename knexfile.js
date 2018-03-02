e2f87e35a67a6048ff43d5d7a967cfba65b94097
michelle moyer <moyer.mj@gmail.com>
2018-03-01 14:42:58 -1000

    created users table

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,7 @@
 // Update with your config settings.
 
 const config = require('./config/config')
+const path = require(`path`);
 
 module.exports = {
 
@@ -17,7 +18,7 @@ module.exports = {
       directory: path.join(__dirname,  'db/migrations')
     },
     seeds: {
-      directory: path.join(__dirname + 'db/seeds')
+      directory: path.join(__dirname, 'db/seeds')
     },
     debug: true
   },
