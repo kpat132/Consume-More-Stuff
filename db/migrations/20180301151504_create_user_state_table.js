822692f335c1fd3d4987eb2cb18404240e5c1c1e
michelle moyer <moyer.mj@gmail.com>
2018-03-01 15:18:13 -1000

    created user/item status tables

---
+++ db/migrations/20180301151504_create_user_state_table.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('user_status', table => {
+    table.increments('id').notNullable();
+    table.enu('status', ['active', 'inactive']);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('user_status')
+};
---
+++ db/migrations/20180301151712_create_item_status_table.js
@@ -0,0 +1,10 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('item_status', table => {
+    table.increments('id').notNullable();
+    table.enu('status', ['published', 'sold']);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('item_status')
+};
