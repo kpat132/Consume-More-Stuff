cbb5b0f0c256b004ed78ae4278089afb62766d61
michelle moyer <moyer.mj@gmail.com>
2018-03-06 08:55:42 -1000

    updating routes more with passport

--- routes/helper.js
+++ routes/helper.js
@@ -8,3 +8,9 @@ function isAuthenticated (req, res, next) {
   if(req.isAuthenticated()) { next();}
   else { res.redirect('/'); }
 }
+
+function isAuthorized (user, param) {
+  if (user !== parseInt(param)) {
+    throw new Error ('User not authorized')
+  }
+}
