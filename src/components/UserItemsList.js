db62355be873056f4602a8c067a55b9941beb511
KrishnaP <kpat132@gmail.com>
2018-03-07 15:36:54 -1000

    fixes

--- src/components/UserItemsList.js
+++ src/components/UserItemsList.js
@@ -20,19 +20,19 @@ const UserItemsList = props => {
           return (
             <ul key={element.name}>
               <li  className='useritemName' > {element.name} </li>
-              <br />
+             
               {description && <li>Description: {description}</li>}
-              <br />
+            
               {price && <li>Price: {price}</li>}
-              <br />
+             
               {make && <li>Make: {make}</li>}
-              <br />
+             
               {model && <li>Model: {model}</li>}
-              <br />
+            
               {dimensions && <li>Dimensions: {dimensions}</li>}
-              <br />
+           
               {image && <li>Image: {image}</li>}
-              <br />
+            
               {notes && <li>Notes: {notes}</li>}
 
             </ul>
